0d39d6f04745058f1f01baf0120b30d41bcc3cbe
Erkki Lindpere <villane@gmail.com>
2013-06-10 16:47:52 +0300

    \ as alias to λ

--- src/lambda/LambdaParser.scala
+++ src/lambda/LambdaParser.scala
@@ -7,11 +7,11 @@ import scala.util.parsing.combinator.PackratParsers
 class LambdaParser extends StdTokenParsers with PackratParsers {
   type Tokens = StdLexical
   val lexical = new LambdaLexer
-  lexical.delimiters ++= Seq("λ", ".", "(", ")", "\\")
+  lexical.delimiters ++= Seq("\\", "λ", ".", "(", ")", "\\")
 
   type P[+T] = PackratParser[T]
   lazy val expr: P[Expr]         = lambda | application | variable | parens
-  lazy val lambda: P[Lambda]     = "λ" ~> variable ~ "." ~ expr ^^
+  lazy val lambda: P[Lambda]     = ("λ" | "\\") ~> variable ~ "." ~ expr ^^
                                    { case v ~ "." ~ e  => Lambda(v, e) }
   lazy val application: P[Apply] = expr ~ expr ^^
                                    { case left ~ right => Apply(left, right) }
