8b6bbbb561ea0b262177e7096b5172b59ddb0d54
Erkki Lindpere <villane@gmail.com>
2013-06-10 16:48:11 +0300

    Pretty printing

--- src/lambda/LambdaREPL.scala
+++ src/lambda/LambdaREPL.scala
@@ -2,13 +2,14 @@ package lambda
 
 object LambdaREPL {
   val parser = new LambdaParser()
+  val pretty = new PrettyPrinter()
 
   def main(args: Array[String]) {
     while (true) {
       val exprSrc = readLine("λ> ")
       import parser.{Success, NoSuccess}
       parser.parse(exprSrc) match {
-        case Success(expr, _) => println("Parsed: " + expr)
+        case Success(expr, _) => println("Parsed: " + pretty(expr))
         case err: NoSuccess   => println(err)
       }
     }
---
+++ src/lambda/PrettyPrinter.scala
@@ -0,0 +1,18 @@
+package lambda
+
+class PrettyPrinter {
+  def apply(expr: Expr): String = expr match {
+    case Lambda(arg, body) => p"λ$arg.$body"
+    case Apply(fun, arg)   => p"$fun $arg"
+    case Var(name)         => s"$name"
+  }
+ 
+  implicit class PrettyPrinting(val sc: StringContext) {
+    def p(args: Expr*) = sc.s((args map parensIfNeeded):_*)
+  }
+ 
+  def parensIfNeeded(expr: Expr) = expr match {
+    case Var(name) => name
+    case _         => "(" + apply(expr) + ")"
+  }
+}
