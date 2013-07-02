06328c677eb1a276596a32248114e2850ea54b00
Erkki Lindpere <villane@gmail.com>
2013-07-02 19:09:38 +0300

    REPL Commands :step <expr>, :debug on/off, :quit

--- src/lambda/LambdaREPL.scala
+++ src/lambda/LambdaREPL.scala
@@ -6,16 +6,42 @@ object LambdaREPL {
   val parser = new LambdaParser()
   val pretty = new PrettyPrinter()
   var bind = new Binder(Library.load())
-  val eval = new Evaluation(debug = false)
+  var eval = new Evaluation(debug = false)
 
   def main(args: Array[String]) {
     while (true) {
       val input = readLine("λ> ")
-      if (input contains "=")
+      if (input startsWith ":")
+        handleCommand(input substring 1)
+      else if (input contains "=")
         handleDef(input)
       else
         handleExpr(input)
     }
+  }
+
+  def handleCommand(input: String) = input.split(" ", 2) match {
+    case Array("step" | "s", exprSrc) =>
+      parseInput(parser.parse, exprSrc) { expr =>
+        try {
+          val in = bind(expr)
+          println(pretty(in) + " →")
+          val out = eval.evalStep(in)
+          println(pretty(out))
+        } catch {
+          case _: MatchError => println("Cannot reduce further.")
+        }
+      }
+    case Array("debug" | "d", arg) =>
+      arg match {
+        case "on"  => eval = new Evaluation(debug = true)
+        case "off" => eval = new Evaluation(debug = false)
+        case _     => println("Syntax: ':debug off' or ':debug on'")
+      }
+    case Array("quit" | "q") =>
+      System.exit(0)
+    case cmd =>
+      println("Unknown command: " + (cmd mkString " "))
   }
 
   def handleDef(input: String) =
